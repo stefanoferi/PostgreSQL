284585d677ec7a212e371f952303bd3429c7bb42
Stefano Noferi <stefanoferi@noze.it>
2024-02-19 11:24:48 +0100

    added db conn

--- python/src/script.py
+++ python/src/script.py
@@ -5,3 +5,20 @@ USER = os.getenv('API_USER')
 
 print('Hello world, ' + USER)
 print('Hello world 2')
+
+import psycopg2
+
+# Connect to your postgres DB
+conn = psycopg2.connect("host=db dbname=northwind user=postgres password=example")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM us_states")
+
+# Retrieve query results
+records = cur.fetchall()
+
+for record in records:
+    print(record)
